63be1a636a8c3cf0fa23d6d0175837ae08088e78
Lane Katris <lanekatris@gmail.com>
2023-10-11 20:55:03 -0400

    messing around with hostable files

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🧠 Lane's Second Brain",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
